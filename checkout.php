90820691f8b2bf9e07360433d5e58475265a095b
Manish Shrestha <manishshtestha472@gmail.com>
2024-10-21 12:03:56 +0545

    Updated the Checkout Page Layout

--- checkout.php
+++ checkout.php
@@ -46,22 +46,93 @@ $sig = hash_hmac(algo: 'sha256', data: $message, key: $secretKey, binary: true);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HandCrafted:Payment</title>
+    <link rel="stylesheet" href="./STATIC/CSS/design.css">
     <style>
-        
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            width: 100%;
+        }
+
+        .container1 {
+            width: 90%;
+            height: 100vh;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            color: aliceblue;
+
+            .cont-left {
+                padding-top: 50px;
+                width: 78%;
+                .list_view{
+                    margin-top: 10px;
+                    height: 80vh;
+                    overflow-y: auto;
+                    overflow-x: hidden;
+                    /* background-color: aliceblue; */
+                    display: flex;
+                    flex-direction: column;
+                    gap: 5px;
+                    padding: 5px;
+                    .list_item{
+                        height: 10vh;
+                        width: 100%;
+                        background-color: rgba(25, 25, 25,0.7);
+                        display: flex;
+                        align-items: center;
+                        gap: 10px;
+                        padding-left:10px;
+                        img{
+                            width: 50px;
+                            height: 50px;
+                        }
+                    }
+                }
+            }
+
+            .cont-right {
+                padding-top: 200px;
+                width: 20%;
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
+
+                h3 {
+                    display: flex;
+                    justify-content: space-between;
+                }
+            }
+        }
     </style>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container1">
 
         <div class="cont-left">
-            <?php if (isset($_SESSION['user_id'])) { ?>
-                <h2>Welcome, <?php $_SESSION['username'] ?></h2>
-            <?php } ?>
-            <p>Order Details</p>
-
+            <h2>Order Details</h2>
+            <hr>
+            <div class="list_view">
+                <?php foreach ($_SESSION['cart'] as $key => $product) { ?>
+                    <div class="list_item">
+                        <img src="./uploads/<?php echo $product['main_img']?>" alt="" srcset="">
+                        <h3><?php echo $product['product_name']?></h3>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
         <div class="cont-right">
+            <h2>Order Summary</h2>
+            <br>
+            <h3><span>Subtotal</span><span>Rs. <?php echo $subtotal; ?></span></h3>
+            <h3><span>Tax(5%)</span><span>Rs. <?php echo $tax ?></span></h3>
+            <h3><span>Shipping</span><span>Rs. <?php echo $shipping ?></span></h3>
+            <hr>
+            <h3><span>Total</span><span>Rs. <?php echo $total ?></span></h3>
             <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
                 <input type="text" id="amount" name="amount" value="<?php echo $subtotal ?>" required hidden>
                 <input type="text" id="tax_amount" name="tax_amount" value="<?php echo $tax ?>" required hidden>
@@ -70,12 +141,12 @@ $sig = hash_hmac(algo: 'sha256', data: $message, key: $secretKey, binary: true);
                 <input type="text" id="product_code" name="product_code" value="EPAYTEST" required hidden>
                 <input type="text" id="product_service_charge" name="product_service_charge" value="0" required hidden>
                 <input type="text" id="product_delivery_charge" name="product_delivery_charge" value="<?php echo $shipping ?>" required hidden>
-                <input type="text" id="success_url" name="success_url" value="https://esewa.com.np" required hidden>
-                <input type="text" id="failure_url" name="failure_url" value="https://google.com" required hidden>
+                <input type="text" id="success_url" name="success_url" value="http://localhost/handicraftmp/status_success.php" required hidden>
+                <input type="text" id="failure_url" name="failure_url" value="http://localhost/handicraftmp/status_fail.php" required hidden>
                 <input type="text" id="signed_field_names" name="signed_field_names" value="total_amount,transaction_uuid,product_code" required hidden>
                 <input type="text" id="signature" name="signature" value="<?php echo base64_encode($sig) ?>" required hidden>
                 <!-- <input value=" Submit" type="submit"> -->
-                <button style="margin-left: 75px;" value="Submit" class="btn btn-fw">
+                <button style="margin-left: 75px;margin-top:10px" value="Submit" class="btn btn-fw">
                     <span class="button_top">Pay Now
                     </span>
                 </button>
